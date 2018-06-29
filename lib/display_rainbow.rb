32c14afea9f48cbd5541ee8e9bfc435a4d0432e3
Rodolfo Guzman <rodolfoguzman147@gmail.com>
2018-06-29 19:12:32 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,5 @@ whats_up=['red','orange','yellow','green','blue','indigo','violet']
 
 
 def display_rainbow(array_of_colors)
-  puts "R: #{array_of_colors[0]}, O: #{array_of_colors[1]}, Y: #{array_of_colors[2]}, G: #{array_of_colors[2]}, B: #{array_of_colors[2]}, I: #{array_of_colors[2]}, V: #{array_of_colors[2]}"
+  puts "R: #{array_of_colors[0]}, O: #{array_of_colors[1]}, Y: #{array_of_colors[2]}, G: #{array_of_colors[3]}, B: #{array_of_colors[4]}, I: #{array_of_colors[5]}, V: #{array_of_colors[6]}"
 end
